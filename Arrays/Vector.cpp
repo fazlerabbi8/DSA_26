2a8ed88097087ff6f698c7cc0e58fdd5d227ede1
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-01 22:58:18 +0600

    Initializes vector using initializer list.

--- Arrays/Vector.cpp
+++ Arrays/Vector.cpp
@@ -8,4 +8,10 @@ int main(){
     vector<int> arr2(5,5);
     for(int x: arr2)
     cout << x << " ";
+
+    cout << "\n";
+
+    vector<int> arr3 = {2, 3, 4, 5};
+    for(int x : arr3)
+    cout << x << " ";
 }
