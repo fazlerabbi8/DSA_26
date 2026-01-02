e46491d888441d41503368f41d8159889dbf3e9c
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-02 23:06:24 +0600

    swap element of vector.

--- Arrays/Vector.cpp
+++ Arrays/Vector.cpp
@@ -12,7 +12,7 @@ int main(){
 
     // cout << "\n";
 
-    // vector<int> arr3 = {2, 3, 4, 5};
+    vector<int> arr3 = {2, 3, 4, 5};
     // for(int x : arr3)
     // cout << x << " ";
 
@@ -24,9 +24,11 @@ int main(){
     arr4.emplace ( it, 200 );
     arr4.emplace_back(500);
 
+
     arr4.pop_back();
     arr4.erase(remove(arr4.begin(), arr4.end(), 2), arr4.end());
-    cout << arr4.at(2) << "\n";
+    // cout << arr4.at(2) << "\n";
+    arr3.swap(arr4);
     for(int x : arr4)
     cout << x << " ";
 
