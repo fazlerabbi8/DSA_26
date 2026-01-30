592c7465c62d7baf90d5d401d2b50b6a843a25e5
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-30 12:28:51 +0600

    A subarray is a contiguous part of the array. An array that is inside another array.

---
+++ ArrayAndString/SubArray.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void subarray(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << arr[k] << " ";
+            }
+            cout << "\n";
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    subarray(arr, n);
+
+    return 0;
+}
