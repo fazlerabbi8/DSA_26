f99ab139726f326fff485ebca1a28e512038fe10
fazlerabbi13 <mohammad0132794@gmail.com>
2026-05-16 17:53:17 +0600

    call push() and displayStack() function in main function.

--- Stack/arrayImplementation.c
+++ Stack/arrayImplementation.c
@@ -4,56 +4,70 @@
 
 typedef struct
 {
-   int top;
-   int capacity;
-   int *array;
+    int top;
+    int capacity;
+    int *array;
 } Stack;
 
-
-Stack*  createStack(int capacity)
+Stack *createStack(int capacity)
 {
-    Stack* stack = (Stack*)malloc(sizeof(Stack));
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
     stack->capacity = capacity;
     stack->top = -1;
-    stack->array = (int*)malloc(stack->capacity * sizeof(int));
+    stack->array = (int *)malloc(stack->capacity * sizeof(int));
     return stack;
 }
 
-int isFull(Stack* stack)
+int isFull(Stack *stack)
 {
     return stack->top == stack->capacity - 1;
 }
 
-int isEmpty(Stack* stack)
+int isEmpty(Stack *stack)
 {
-    return stack->top = -1;
+    return stack->top == -1;
 }
 
-void push(Stack* stack, int value)
+void push(Stack *stack, int value)
 {
-    if(isFull(stack))
+    if (isFull(stack))
     {
         printf("Stack Overflow! Stack is full.\n");
         return;
     }
 
     stack->array[++stack->top] = value;
+    printf("%d pushed to stack\n", value);
 }
 
-
-void displayStack(Stack* stack)
+void displayStack(Stack *stack)
 {
-    if(isEmpty(stack))
+    if (isEmpty(stack))
     {
         printf("Stack is empty\n");
     }
     else
     {
-        for(int x = stack->top; i >= 0; i--)
+        for (int x = stack->top; x >= 0; x--)
         {
-            printf("%d\n", stack->array[i]);
+            printf("%d\n", stack->array[x]);
         }
     }
 }
 
+int main()
+{
+    Stack *stack = createStack(50);
 
+    push(stack, 5);
+    push(stack, 6);
+    push(stack, 7);
+    push(stack, 8);
+    push(stack, 9);
+
+    printf("-----------\n");
+
+    displayStack(stack);
+
+    return 0;
+}
